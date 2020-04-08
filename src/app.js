5dda2dac5deca5cd4e1dac7474990856cbc5e762
Edmundo-Ribeiro <jtvedy@gmail.com>
2020-04-08 00:14:56 -0300

    implementation of all routes according to the tests demands

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,53 @@ app.use(cors());
 
 const repositories = [];
 
+
+function checkValidID(request, response, next){
+  const repIndex = repositories.findIndex( ({id})=> id === request.params.id );
+  
+  if(repIndex<0){
+    return response.status(400).json({error:'There is no repository with this ID'})
+  }
+  request.locals = {repIndex};
+  return next();
+}
+
+app.use('/repositories/:id', checkValidID);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const content = request.body;
+  const newRepository = {id: uuid(), likes: 0, ...content};
+  repositories.push(newRepository);
+  return response.json(newRepository);
 });
+
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const updatableFilds = ['url','title','techs'];
+  const newInfo = request.body;
+  const repIndex = request.locals.repIndex;
+  
+  for(let fild of updatableFilds){
+    repositories[repIndex][fild] = newInfo[fild] || repositories[repIndex][fild];
+  }
+
+  return response.json(repositories[repIndex]);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const repIndex = request.locals.repIndex;
+  repositories.splice(repIndex,1);
+  return  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repIndex = request.locals.repIndex;
+  repositories[repIndex].likes+=1;
+  return response.json(repositories[repIndex]);
 });
 
 module.exports = app;
